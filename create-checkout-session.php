3cb9d924290a02f50c43d175a037d3acbb382552
AITS\nambinina2 <fetra.harinjatovo@axian-group.com>
2021-07-22 17:07:19 +0300

    host for stripe

--- create-checkout-session.php
+++ create-checkout-session.php
@@ -1,12 +1,12 @@
 <?php
 require './parameters.php';
 require 'vendor/autoload.php';
-//\Stripe\Stripe::setApiKey('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');
- \Stripe\Stripe::setApiKey('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+\Stripe\Stripe::setApiKey('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');
+// \Stripe\Stripe::setApiKey('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 header('Content-Type: application/json');
 
-$MY_DOMAIN = 'http://localhost:3000';
+$MY_DOMAIN = 'http://demarches-publiques.com';
 $tableName = '';
 
 
